0d7a7bc69ff912786f53529801a66dfbf36d1aef
蔡灼炫 <14753716+cai-zhuoxuan@user.noreply.gitee.com>
2024-11-09 18:09:32 +0800

    feat: 项目初始化依赖

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+//@ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+const app = createApp(App)
+app.use(ElementPlus, {
+    locale: zhCn  //  element-plus国际化
+})
+
+app.mount('#app')
